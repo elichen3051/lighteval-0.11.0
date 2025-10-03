a83c8fb8648e5da535acd198cd022767be0870ec
elichen3051 <157401253+elichen3051@users.noreply.github.com>
2025-10-03 13:57:28 +0800

    Update vllm_model.py

--- src/lighteval/models/vllm/vllm_model.py
+++ src/lighteval/models/vllm/vllm_model.py
@@ -441,7 +441,7 @@ class VLLMModel(LightevalModel):
             @ray.remote(num_gpus=self.tensor_parallel_size)
             def run_inference_one_model(model_args: dict, sampling_params: SamplingParams, requests):
                 llm = LLM(**model_args)
-                return llm.generate(prompt_token_ids=requests, sampling_params=sampling_params)
+                return llm.generate(requests, sampling_params=sampling_params)
 
             # dispatch requests to all self.data_parallel_size workers, in interleaved fashion
             # interleaved important to balance context lengths across workers
@@ -459,7 +459,7 @@ class VLLMModel(LightevalModel):
             ]
         else:
             outputs = self.model.generate(
-                prompts=inputs,
+                inputs,
                 sampling_params=sampling_params,
                 use_tqdm=True,
             )
